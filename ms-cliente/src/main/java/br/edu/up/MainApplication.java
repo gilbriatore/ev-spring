4d17cde18cfc3fe23654ac24d489f8ceba819d1e
geucimar <geucimar@gmail.com>
2022-06-24 20:32:33 -0300

    Cliente

--- ms-cliente/src/main/java/br/edu/up/MainApplication.java
+++ ms-cliente/src/main/java/br/edu/up/MainApplication.java
@@ -2,10 +2,17 @@ package br.edu.up;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@RestController
 @SpringBootApplication
 public class MainApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	String home(){
+		return "Microserviço Cliente";
 	}
 }
